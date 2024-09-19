5241181f6b47db2998589c7cb267277e979e9817
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-09-19 16:14:13 -0500

    modified:   src/main/java/com/example/antiguedades/entities/persona/domain/entity/Persona.java

--- src/main/java/com/example/antiguedades/entities/persona/domain/entity/Persona.java
+++ src/main/java/com/example/antiguedades/entities/persona/domain/entity/Persona.java
@@ -2,6 +2,7 @@ package com.example.antiguedades.entities.persona.domain.entity;
 
 import com.example.antiguedades.entities.estadopersona.domain.entity.EstadoPersona;
 import com.example.antiguedades.entities.genero.domain.entity.Genero;
+import com.example.antiguedades.entities.tipopersona.domain.entity.TipoPersona;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
